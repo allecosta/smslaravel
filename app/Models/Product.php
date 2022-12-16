267bb9d7d18ecd0a10af53031da4c460b4565350
allecosta <alle_costa10@outlook.com>
2022-12-16 15:54:57 -0300

    add attributes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,10 @@ class Product extends Model
 
     // caso a propriedade não seja especificada, o laravel irá considerá o nome da classe como sendo o nome da tabela
     protected $table = "products";
+
+    public $timestamps = false;
+    protected $fillable = ['name', 'price', 'description', 'quantity'];
+
+    // impedindo que o usuário altere o ID do Model
+    protected $guarded = ['id'];
 }
