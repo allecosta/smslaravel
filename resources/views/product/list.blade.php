ae22d8c9b0f055769cb7322664091646cfaf7906
allecosta <alle_costa10@outlook.com>
2023-01-26 10:48:18 -0300

    feat: inserting button icons and updating table

--- resources/views/product/list.blade.php
+++ resources/views/product/list.blade.php
@@ -14,12 +14,13 @@
             O produto <strong>{{ old('name') }}</strong> foi adicionado com sucesso!
         </div>
     @endif
-        <table class="table table-striped table-hover">
+        <table class="table table-striped table-bordered">
             <tr style="text-align: center;">
                 <th>Produto</th>
                 <th>Valor</th>
                 <th>Descrição</th>
                 <th>Quantidade</th>
+                {{-- <th>Ação</th> --}}
             </tr>
             @foreach ($products as $product)
                 <tr class="{{ $product->quantity <= 1 ? 'alert alert-danger' : '' }}">
@@ -27,14 +28,18 @@
                     <td>{{ $product->price }}</td>
                     <td>{{ $product->description }}</td>
                     <td>{{ $product->quantity }}</td>
-                </tr>
-                <tr>
                     <td>
-                        <a href="/products/display/{{$product->id}}">
-                            Visualizar
+                        <a href="{{action('App\Http\Controllers\ProductController@display', $product->id)}}">
+                            <button type="button" class="btn btn-primary btn-sm m-2">
+                                <i class="fa fa-eye" aria-hidden="true"></i>
+                            </button>
                         </a>
+                    </td>
+                    <td>
                         <a href="{{action('App\Http\Controllers\ProductController@remove', $product->id)}}">
-                            Excluir
+                            <button type="button" class="btn btn-danger btn-sm m-2">
+                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                            </button>   
                         </a>
                     </td>
                 </tr>
